0ce669f95893f2705fa35633bdfe60782400c1e5
josheisy500 <josheisy500@gmail.com>
2015-03-30 11:57:44 +0100

    30/3/15

--- Java Servlets/CreateProfile.java
+++ Java Servlets/CreateProfile.java
@@ -36,31 +36,6 @@ public class CreateProfile extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		// Set response content type
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		String title = "";
-
-		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 "
-				+ "transitional//en\">\n";
-		out.println(docType + "<html>\n" + "<head><title>" + title
-				+ "</title></head>\n" + "<body bgcolor=\"#f0f0f0\">\n"
-				+ "<h1 align=\"center\">" + title + "</h1>\n" + "<ul>\n" +
-
-				"  <li><b>Anonymous User Name</b>: "
-				+ request.getParameter("anonUsrNm") + "\n" +
-
-				"  <li><b>Longitude</b>: " + request.getParameter("long")
-				+ "\n" +
-
-				"  <li><b>Latitude</b>: " + request.getParameter("lat")
-				+ "\n" +
-
-				"  <li><b>Current Interest</b>: "
-				+ request.getParameter("crntItst")
-
-				+ "</body></html>");
-
 	}
 
 	protected void doPost(HttpServletRequest request,
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 socialProx
 ==========
+
+Mobile Application 
+
+Offers a unique social networking experience based on users current interest and their current location.
+
+Developed using the Titanium Appcelerator IDE (JavaScript, HTML5, CSS3)
+
+Using Java Servlets running on Apache Tomcat (an open source web server and servlet container) 
+the application can collect input from mobile devices through web page forms, present and stroe records to and from a MySQL database.   
